f3f3c40c53c7b0b45afbcbd9705640336316c36d
Eric Rochester <erochest@virginia.edu>
2012-12-05 21:23:17 -0500

    Added clojure.tools.trace to default user dependencies in lein config.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,4 @@
 {:user {:plugins [[lein-localrepo "0.4.1"]
                   [lein-pprint "1.1.1"]
-                  [lein-oneoff "0.2.0"]]}}
+                  [lein-oneoff "0.2.0"]]
+        :dependencies [[org.clojure/tools.trace "0.7.5"]]}}
