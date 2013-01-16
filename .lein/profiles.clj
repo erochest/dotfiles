11156a1dcea0391a0cc57b59f263232988cdcba8
Eric Rochester <erochest@virginia.edu>
2013-01-16 10:30:28 -0500

    Added lein-outdated to my user profile.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,5 @@
 {:user {:plugins [[lein-localrepo "0.4.1"]
                   [lein-pprint "1.1.1"]
-                  [lein-oneoff "0.2.0"]]
+                  [lein-oneoff "0.2.0"]
+                  [lein-outdated "1.0.0"]]
         :dependencies [[org.clojure/tools.trace "0.7.5"]]}}
