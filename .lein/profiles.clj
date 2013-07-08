aec8056836cb95eedb54c2f3818e19bbf493568c
Eric Rochester <erochest@virginia.edu>
2013-07-08 11:34:54 -0400

    Added lein depgraph module.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,6 @@
 {:user {:plugins [[lein-localrepo "0.4.1"]
                   [lein-pprint "1.1.1"]
                   [lein-oneoff "0.2.0"]
-                  [lein-outdated "1.0.0"]]
+                  [lein-outdated "1.0.0"]
+                  [lein-depgraph "0.1.0"]]
         :dependencies [[org.clojure/tools.trace "0.7.5"]]}}
