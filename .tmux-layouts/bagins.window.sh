feda75cf818e577bc8d44dce05ef1ad80c8454de
Eric Rochester <erochest@virginia.edu>
2013-09-19 14:48:09 -0400

    Fixed directory in bagins layout.

--- .tmux-layouts/bagins.window.sh
+++ .tmux-layouts/bagins.window.sh
@@ -1,7 +1,8 @@
 # Set window root path. Default is `$session_root`.
 # Must be called before `new_window`.
 # window_root "~/p/bagins/src/github.com/APTrust/bagins/"
-window_root "~/p/bagins/"
+# window_root "~/p/bagins/"
+window_root "~/p/bagins/bagger/"
 
 # Create new window. If no argument is given, window name will be based on
 # layout file name.
