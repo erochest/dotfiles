6f5729f0982b5a1c78b77d8d07af3db851d96fb8
Eric Rochester <erochest@virginia.edu>
2013-09-23 13:50:58 -0400

    Updated working directory for cljdata tmux layout.

--- .tmux-layouts/cljdata.window.sh
+++ .tmux-layouts/cljdata.window.sh
@@ -1,6 +1,6 @@
 # Set window root path. Default is `$session_root`.
 # Must be called before `new_window`.
-window_root "~/p/cljdata/sentiment/"
+window_root "~/p/cljdata/nullh/"
 
 # Create new window. If no argument is given, window name will be based on
 # layout file name.
