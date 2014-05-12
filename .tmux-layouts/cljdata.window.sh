1489973ada60e745c91f7d89cc85232b12141f00
Eric Rochester <erochest@virginia.edu>
2014-05-12 09:33:11 -0400

    weekly update Mon May 12 09:33:10 EDT 2014

--- .tmux-layouts/cljdata.window.sh
+++ .tmux-layouts/cljdata.window.sh
@@ -1,6 +1,6 @@
 # Set window root path. Default is `$session_root`.
 # Must be called before `new_window`.
-window_root "~/p/cljdata/"
+window_root "~/p/cljdata/network-six/"
 
 # Create new window. If no argument is given, window name will be based on
 # layout file name.
