bc653dc257ce5af0b31f571ccdba80aedbecc483
Eric Rochester <erochest@virginia.edu>
2014-11-11 08:57:56 -0500

    weekly update Tue Nov 11 08:57:55 EST 2014

--- .tmux-layouts/orc.window.sh
+++ .tmux-layouts/orc.window.sh
@@ -1,6 +1,6 @@
 # Set window root path. Default is `$session_root`.
 # Must be called before `new_window`.
-window_root "~/p/orcbot"
+window_root "~/p/orc"
 
 # Create new window. If no argument is given, window name will be based on
 # layout file name.
--- .tmux-layouts/ribot.window.sh
+++ .tmux-layouts/ribot.window.sh
@@ -3,16 +3,13 @@ new_window "ribot"
 
 split_h 50
 split_v 66
-split_v 50
 
 send_keys "" 0
 send_keys "" 1
 send_keys "" 2
-send_keys "" 3
 
 run_cmd "svim" 0
-run_cmd "guard" 1
-run_cmd "cabal repl" 2
-run_cmd ls 3
+run_cmd "cabal repl" 1
+run_cmd ls 2
 
 select_pane 0
