e59cfa90ab44ed14313b41628606df8f405d91e9
Eric Rochester <erochest@virginia.edu>
2015-03-24 10:46:02 -0400

    Added layout for journal project.

---
+++ .tmux-layouts/journal.window.sh
@@ -0,0 +1,22 @@
+# Set window root path. Default is `$session_root`.
+# Must be called before `new_window`.
+window_root "~/p//journal"
+
+# Create new window. If no argument is given, window name will be based on
+# layout file name.
+new_window "journal"
+
+# Split window into panes.
+split_h 50
+split_v 66
+
+send_keys "" 0
+send_keys "" 1
+send_keys "" 2
+
+run_cmd "svim"       0
+run_cmd "gulp watch" 1
+run_cmd "ls"         2
+
+# Set active pane.
+select_pane 0
